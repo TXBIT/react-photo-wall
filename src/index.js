c16ab81f64ef781e27c79859820fe72106665991
TXBIT <shenlong203@gmail.com>
2020-07-20 16:14:30 +0700

    007 Component State Navigation

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import Main from './components/Main';
 import './styles/stylesheet.css';
 
-ReactDOM.render(<Main />, document.getElementById('root'));
+ReactDOM.render(
+  <BrowserRouter>
+    <Main />
+  </BrowserRouter>,
+  document.getElementById('root')
+);
